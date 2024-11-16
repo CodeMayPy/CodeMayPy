ead043e6d85f8c2c1623acde5f9c850d114065a1
Mayara <code.maypy@gmail.com>
2024-11-16 12:17:56 -0300

    atualizado

--- ex003.py
+++ ex003.py
@@ -1,4 +1,4 @@
-# Crie uma progama que leia dois números e mostre a soma deles.
+# Crie uma programa que leia dois números e mostre a soma deles.
 n1 = int(input('Digite um valor:'))
 n2 = int(input('Digite outro valor:'))
 print(f'A soma de {n1} + {n2} é igual a {n1+n2}.')
