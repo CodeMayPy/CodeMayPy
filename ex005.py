9d0a0dccaf6beb08fd56504163b575a379d9e58b
Mayara <code.maypy@gmail.com>
2024-11-17 18:56:41 -0300

    atualizado

--- ex005.py
+++ ex005.py
@@ -1,3 +1,3 @@
 # Crie um programa que leia um número e mostre seu antecessor e seu sucessor.
 n = int(input('Digite um número:'.strip()))
-print(f'Se o número escolhido foi {n}, seu antecessor é {(n-1)} e seu sucessor é {(n+1)}.')
+print(f'Se o número escolhido foi \033[35m{n}\033[m, seu antecessor é \033[31m{(n-1)}\033[m e seu sucessor é \033[32m{(n+1)}\033[m.')
