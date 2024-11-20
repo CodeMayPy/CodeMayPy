e170c9dc78abb3c09eb219b2d3f9de01e72ff422
Mayara <code.maypy@gmail.com>
2024-11-20 18:39:50 -0300

    Create ex056.py

---
+++ ex056.py
@@ -0,0 +1,25 @@
+#Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade
+#do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+soma_idade = 0
+media_idade = 0
+maior_idade_homem = 0
+nome_velho = ' '
+tot_mulher20 = 0
+for p in range(1, 5):
+    print(f'---- {p}ª pessoa ----')
+    nome = str(input('Nome:')).strip()
+    idade = int(input('Idade:'))
+    sexo = str(input('Sexo: [M/F]:')).strip()
+    soma_idade += idade
+    if p == 1 and sexo in "Mm":
+        maior_idade_homem = idade
+        nome_velho = nome
+    if sexo in 'Mm' and idade > maior_idade_homem:
+        maior_idade_homem = idade
+        nome_velho = nome
+    if sexo in 'Ff' and idade < 20:
+            tot_mulher20 += 1
+media_idade = soma_idade / 4
+print(f'A média de idade do grupo é de {media_idade} anos.')
+print(f'O homem mais velho tem {maior_idade_homem} e se chama {nome_velho}.')
+print(f'Ao todo são {tot_mulher20} mulheres com menos de 20 anos.')
