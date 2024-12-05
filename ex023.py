d63b3e70e01b128a097123c69710e56f1a52c4aa
Mayara <code.maypy@gmail.com>
2024-12-05 19:35:06 -0300

    Update ex023.py

--- ex023.py
+++ ex023.py
@@ -4,8 +4,8 @@ u = num // 1 % 10
 d = num // 10 % 10
 c = num // 100 % 10
 m = num // 1000 % 10
-print('Analisando o número {}'.format(num))
-print('Sua unidade é {}.'.format(u))
-print('Sua dezena é {}.'.format(d))
-print('Sua centena é {}.'.format(c))
-print('Seu milhar é {}.'.format(m))
+print(f'Analisando o número {num}.')
+print(f'Sua unidade é {u}.')
+print(f'Sua dezena é {d}.')
+print(f'Sua centena é {c}.')
+print(f'Seu milhar é {m}.')
