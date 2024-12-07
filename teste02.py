f8848635789ec2244d6ac0c24b353f28a58fc60b
Mayara <code.maypy@gmail.com>
2024-12-07 18:35:55 -0300

    testes

---
+++ teste02.py
@@ -0,0 +1,4 @@
+dia = input('Qual seu dia de nascimento?')
+mes = input('Qual seu m~es de nascimento?')
+ano = input('Qual seu ano de nascimento?')
+print(f'Você nasceu em {dia}, de {mes} de {ano}, certo?')
