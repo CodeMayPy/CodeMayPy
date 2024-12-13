2dbf4152b476cd7918447829a42e9ada55ff1ad1
Mayara <code.maypy@gmail.com>
2024-12-13 15:11:01 -0300

    New exercise.

---
+++ ex057.py
@@ -0,0 +1,6 @@
+# Make a program that reads a person's gender, but only accepts the values ​​'M' or 'F'. If it's wrong, ask
+# typing again until you have a correct value.
+gender = str(input('What is your gender?[F/M]:')).strip().upper()[0]
+while gender not in 'MmFf':
+    gender = str(input('Invalid data!! Please enter your gender:')).strip().upper[0]
+print(f'Gender {gender} successfully registered.')
