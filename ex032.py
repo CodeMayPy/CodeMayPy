b715a621fc9abf021a4741015af8669db3a5fe57
Mayara <code.maypy@gmail.com>
2024-12-14 19:11:06 -0300

    Updated

--- ex032.py
+++ ex032.py
@@ -1,6 +1,6 @@
-# Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
-ano = int(input('Qual ano deseja analizar?'))
-if ano % 4 == 0 and ano % 100 !=0 or ano % 400 ==0:
-    print('O ano {} é bissexto'.format(ano))
+# Make a program that reads any year and shows whether it is a leap year.
+year = int(input('Which year do you want to analize?'))
+if year % 4 == 0 and year % 100 !=0 or year % 400 ==0:
+    print(f"The year {year} it's a leap year")
 else:
-    print('O ano {} NÃO é bissexto.'.format(ano))
+    print(f"The year {year} it's not a leap year")
