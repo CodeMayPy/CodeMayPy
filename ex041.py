c28a62625a52d27c656e4e218794ed3d877ca230
Mayara <code.maypy@gmail.com>
2024-12-18 12:06:36 -0300

    Update ex041.py

--- ex041.py
+++ ex041.py
@@ -1,21 +1,21 @@
-#A Confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua
-# categoria, de acordo com a idade:  – Até 9 anos: MIRIM
-#– Até 14 anos: INFANTIL
-#– Até 19 anos: JÚNIOR
-#– Até 25 anos: SÊNIOR
-#– Acima de 25 anos: MASTER
+#The National Swimming Confederation needs a program that reads an athlete's birth year and shows their
+# category, according to age: – Up to 9 years old: child
+#– Up to 14 years old: children's
+#– Up to 19 years old: JUNIOR
+#– Up to 25 years old: SENIOR
+#– Over 25 years old: MASTER
 from datetime import date
 atual = date.today().year
-nasc = int(input('Qual o ano do nascimento do atleta?'))
-idade = atual - nasc
-print('O atleta nasceu em {} e tem {}anos.'.format(nasc, idade))
-if idade <= 9:
-    print('Sua categoria é a MIRIM!')
-if idade >= 9 and idade < 14:
-    print('Sua categoria é a INFANTIL!')
-if idade >= 14 and idade <= 19:
-    print('Sua categoria é a JÚNIOR!')
-if idade >= 19 and idade <= 25:
-    print('Sua categoria é a SÊNIOR!')
-elif idade >=25:
-    print('Sua categoria é a MASTER!')
+birth = int(input('What year was the athlete born?'))
+age = atual - birth
+print(F'the athlete was born in {birth} and has {age} years old.')
+if age <= 9:
+    print('Your category is CHILD!')
+if age >= 10 and age < 14:
+    print("Your category is CHILDREN'S.")
+if age >= 15 and age <= 19:
+    print('Your category is JUNIOR!')
+if age >= 20 and age <= 25:
+    print('Your category is SENIOR!')
+elif age >=26:
+    print('Your category is MASTER!')
