4de740b2c6b71bfc67619766c9be3c6f147de433
Mayara <code.maypy@gmail.com>
2024-12-18 11:20:22 -0300

    updated

--- ex040.py
+++ ex040.py
@@ -1,15 +1,14 @@
-# Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo
-# com a média atingida:
-# – Média abaixo de 5.0: REPROVADO
-# – Média entre 5.0 e 6.9: RECUPERAÇÃO
-# – Média 7.0 ou superior: APROVADO
-n1 = float(input('Digite sua nota:'))
-n2 = float(input('Digite sua segunda nota:'))
-media = ( n1 + n2 ) / 2
-print('A sua média é {}.'.format(media))
-if media < 5.0:
-    print('Você foi REPROVADO!')
-if media > 5.0:
-    print('Você esta de REUPERAÇÂO!')
-if media >= 6.9:
-    print('Parabéns você passou!')
+# Create a program that reads two grades from a student and calculates their average, displaying a message at the end, according to the acquired media:
+# – Media below 5.0: FAILED
+# – AVERAGE BETWEEN 5.0 and 6.9: RECOVERED
+# – Media 7.0 or higher: PASSED
+first_note = float(input('Enter your note:'))
+second_note = float(input('Enter the second note:'))
+grade = ( first_note + second_note ) / 2
+print(f'Your avarege is {grade}.')
+if grade < 5.0:
+    print('You are FAILED!')
+if grade > 5.0:
+    print('You are RECOVERED!')
+if grade >= 6.9:
+    print('Congratulations you PASSED!')
