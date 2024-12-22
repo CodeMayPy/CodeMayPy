4d17c7e1727d72ea5156c04f36b661d170fa7dac
Mayara <code.maypy@gmail.com>
2024-12-22 13:34:32 -0300

    updated

--- ex037.py
+++ ex037.py
@@ -4,7 +4,7 @@ number= int(input('Enter an integer:'))
 print('''Choose one of the bases for conversion:
 [ 1 ] convert to  BINARY
 [ 2 ] convert to OCTAL
-[ 3 ] convert toHEXADECIMAL''')
+[ 3 ] convert to HEXADECIMAL''')
 option = int(input('your choose:'))
 if option == 1:
     print(f'{number} converted to binary it becomes {bin(number)[2:]}.')
---
+++ ex061.py
@@ -0,0 +1,13 @@
+#  Redo CHALLENGE 51, reading the first term and the reason of an AP, showing the first 10 terms of the progression
+#  using the while structure.
+print("Arithmetic progression generator")
+print("-=-" * 20)
+first_term = int(input("Enter the first term:"))
+reason = int(input("Enter the reason of the arithmetic progression:"))
+term = first_term
+counter = 1
+while counter <= 10:
+    print(f"{term}", end=' ')
+    term += reason
+    counter += 1
+print("End")
