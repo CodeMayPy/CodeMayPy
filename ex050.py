990277c940cd2af1a2822d0fc112cec1786b9065
Mayara <code.maypy@gmail.com>
2025-01-04 21:37:28 -0300

    Refactor: to Portuguese for English.

--- ex050.py
+++ ex050.py
@@ -3,8 +3,8 @@
 sum_numbers = 0
 counter = 0
 for c in range (1, 7):
-    numbers = int(input(f'Enter a {c} value:'))
-    if sum_numbers % 2 == 0:
+    numbers = int(input(f'Enter the {c} value:'))
+    if numbers % 2 == 0:
         sum_numbers += numbers
         counter += 1
-print(f"You informed {counter} neven numbers the sum was {sum_numbers}.")
+print(f"You informed {counter} even numbers and their sum is {sum_numbers}.")
