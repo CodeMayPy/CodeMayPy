f1eda615473d45942cec4cf8409c43e00f53bbd6
Mayara <code.maypy@gmail.com>
2025-01-04 21:37:34 -0300

    novo

---
+++ ex077.py
@@ -0,0 +1,13 @@
+# 077: Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar,
+# para cada palavra, quais são as suas vogais.
+frutas = ('banana', 'maça', 'abacaxi', 'abacate',
+          'limao', 'pera', 'maracuja', 'melancia',
+          'kiwi', 'bergamota', 'ameixa', 'melao')
+for p in frutas:
+    print(f'\n Na palavra {p.upper()} temos ' ,end='')
+    for letra in p:
+        if letra.lower() in "aeiou":
+            print(letra, end=' ')
+
+
+            
