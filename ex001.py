81f48941edc7cceec660d1f1c03cf4a5abde0b61
Mayara <code.maypy@gmail.com>
2025-01-07 13:31:57 -0300

    doc: add comments
    First commands in Pycharm.

--- ex001.py
+++ ex001.py
@@ -1,2 +1,8 @@
 # 01- Create a program that writes "Hello world!" on the screen.
 print('\033[1;32;40m Hello, world!\033[m')
+
+'''PT- Crie um programa que escreva Olá mundo! na tela.
+print(Olá, mundo!)'''
+
+#PT- print vai mostrar na telá a mensagem que você deseja.
+#En- print will show the menssage you want.
