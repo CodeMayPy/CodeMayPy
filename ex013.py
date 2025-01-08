562cca74ef0dcb39bef6d8b86174281ccd1981eb
Mayara <code.maypy@gmail.com>
2025-01-08 17:13:52 -0300

    doc: add comments
    exercises with commented arithmetic operators.

--- ex013.py
+++ ex013.py
@@ -3,3 +3,11 @@ salary = float(input('Enter your salary in R$:'))
 print('Calculating you new salary with a 15% increase...')
 increase = salary + (salary * 0.15)
 print(f'Your new salary will be R${increase}.')
+
+#13: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+'''salario = float(input('Digite o valor do seu salário em R$:'))
+aumento = salario * 0.15
+print(f'Seu salário de R${salario:.2f}, com aumento de 15% ficará R${salario+aumento:.2f}')'''
+
+#PT- mais um treino de operadores aritméticos.
+#EN- another training in arithmetic operators
