5f0bf19aec4538ab5df62a6dbe78c17f4be20696
Mayara <code.maypy@gmail.com>
2025-01-09 19:19:12 -0300

    doc: add comments
    Exercises on modules with comments.

--- ex019.py
+++ ex019.py
@@ -8,3 +8,19 @@ name_four = str(input('Fourth students name:'))
 list = [name_one, name_two, name_three, name_four]
 chosen = random.choice(list)
 print(f'The student chosen to erase the board is {chosen}.')
+
+
+#19: Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o
+#nome dos alunos e escrevendo na tela o nome do escolhido.
+'''import random
+primeiro = str(input('Qual o nome do primeiro aluno:'))
+segundo = str(input('Qual o nome do segundo aluno:'))
+terceiro = str(input('Qual o nome do terceiro aluno:'))
+quarto = str(input('Qual o nome do quarto aluno:'))
+lista = [primeiro, segundo, terceiro, quarto]
+print(f'O aluno escolhido foi: {random.choice(lista)}')'''
+
+#PT- Foi preciso importar o ramdom para misturar os dados da lista (e criar a lista) depois pedir ao programa para escolher
+#um dos dados informados da lista pelo comando choise.
+#EN-It was necessary to import the ramdom to mix the data from the list (and create the list) and then ask the program to
+# choose one of the data provided in the list by the choise command.
--- ex021.py
+++ ex021.py
@@ -5,3 +5,15 @@ pygame.mixer.init()
 pygame.mixer.music.load('nana.mp3')
 pygame.mixer.music.play()
 pygame.quit()
+
+
+#21: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+import pygame
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('nana.mp3')
+pygame.mixer.music.play()
+pygame.quit()
+
+#PT- Treinando o uso de modulos, no caso math.
+#EN- Training the use of modules, in this case math
