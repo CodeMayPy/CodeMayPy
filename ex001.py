c31fbc66c670504cfb9024bc242c11cfc775910f
Mayara <code.maypy@gmail.com>
2025-01-11 13:52:06 -0300

    doc: add comments
    First commands in Pycharm.
    Responding to the user.

--- ex001.py
+++ ex001.py
@@ -1,6 +1,8 @@
+#Responding to the user.
 # 01- Create a program that writes "Hello world!" on the screen.
 print('\033[1;32;40m Hello, world!\033[m')
 
+#Respondendo ao usuário
 '''PT- Crie um programa que escreva Olá mundo! na tela.
 print(Olá, mundo!)'''
 
