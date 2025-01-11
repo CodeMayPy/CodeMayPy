41b020c861db39614c470385a80b6781ae1c5701
Mayara <code.maypy@gmail.com>
2025-01-11 13:57:34 -0300

    doc: add comments
    First commands in Pycharm.
    Respondig to user.

--- ex002.py
+++ ex002.py
@@ -1,7 +1,9 @@
+#responding to user.
 #02- Create a program that asks for the user's name and writes a welcome message.
 name = input('What is your name:')
 print(f'Nice meet you, {name}!')
 
+#respondendo ao usuário.
 '''PT- Crie um programa que pergunte o nome do usuário e mostre uma mensagem de boas vindas.
 nome = input('Qual o seu nome?')
 print(f'É um prazer te conhecer {nome}, sejá bem vindo ao mundo Python')'''
