d972867dfc3f22acfce0ebfd8fc7284073a57ac0
Mayara <code.maypy@gmail.com>
2025-01-13 10:13:38 -0300

    doc: add comments
    Exercises with commented arithmetic operators.
    Painting Wall.

--- ex011.py
+++ ex011.py
@@ -1,3 +1,4 @@
+#Painting Wall.
 #11- Write a program that reads the width and height of a wall in meters, calculates the amount of paint needed to
 #  paint it, knowing that each liter of paint paints an area of 2m².
 area_q = float(input('Enter the height in meters of the wall you want to paint:'))
@@ -6,6 +7,7 @@ area = area_q * width
 liter = area / 2
 print(f'If you have an area of {area}m soon you will need {liter} liters to paint it.')
 
+#Pintando Parede.
 #11: Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta
 # necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
 '''print('Vamos calcular quantos litros de tinta você vai precisar para pintar uma parede?')
