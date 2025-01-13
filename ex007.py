e296e0bdde36bba3582b731095c3c1b29a50cd39
Mayara <code.maypy@gmail.com>
2025-01-13 10:05:55 -0300

    doc: add comments
    Exercises with commented arithmetic operators.
    Arithmetic Mean.

--- ex007.py
+++ ex007.py
@@ -1,8 +1,10 @@
+#Arithmetic Mean
 #07- Develop a program that reads a student's two grades, calculates and displays their average.
 first_note = float(input('Enter your first note:'))
 second_note = float(input('Enter your second note:'))
 print(f' If your first note is {first_note} and your second note is {second_note}.\n You avarege will be {(first_note+second_note)/2}.')
 
+#Média Aritmética.
 '''#7: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 print('Vamos calcular sua média?')
 primeira_nota = float(input('Digite a primeira nota:'))
