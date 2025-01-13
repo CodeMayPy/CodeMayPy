3ba548af458d7114e0b13aec4da51d41d30c3dd9
Mayara <code.maypy@gmail.com>
2025-01-13 14:01:25 -0300

    doc: add comments
    Conditional exercises (if, elif, else).
    Guessing game v1.0.

--- ex028.py
+++ ex028.py
@@ -1,3 +1,4 @@
+#Guessing Game v.1.0
 #28- Write a program that makes the computer "think" of an integer between 0 and 5 and ask the user to try
 # find out which number was chosen by the computer. The program should write on the screen whether the user won or
 # it lost.
@@ -11,3 +12,19 @@ if player == computer:
     print('CONGRATULATIONS! You beat me.')
 else:
     print(f'I WON!!!!! I thought about the number {computer} and not in {player}!')
+
+
+#Jogo da Adivinhação v.1.0
+#28: Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar
+#descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu
+from random import randint
+computador = randint(0,5)
+print('Estou pensanem em um número de 0 a 5, tente adivinhar qual é...')
+jogador = int(input('Qual número eu pensei?'))
+if jogador == computador:
+    print('Parabéns você acertou!')
+else:
+    print(f'Você perdeu. Eu pensei no {computador} e não no {jogador}.')
+
+#PT- Exercícios com uso de condicional.
+#EN- Exercises using conditional.
