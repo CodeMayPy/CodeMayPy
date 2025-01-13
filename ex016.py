650183f6ed45983704ee0ecafc5e63fa307477ec
Mayara <code.maypy@gmail.com>
2025-01-13 10:23:41 -0300

    doc: add comments
    Exercises on modules with comments.
    Breaking down a number.

--- ex016.py
+++ ex016.py
@@ -1,9 +1,11 @@
+#Breaking down a number.
 #16- Create a program that reads any real number from the keyboard and displays its entire portion on the screen.
 import math
 number = float(input('Enter a value:'))
 print('The value typed was {} and its entire part is {}.'.format(number, math.trunc(number)))
 
 
+#Quebrando um número.
 #16: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 '''import math
 numero = float(input('Digite um número com parte fracionária:'))
