e2c4895dd609824a187bdf4c9ec64be5188cdf4d
Mayara <code.maypy@gmail.com>
2025-01-13 10:53:51 -0300

    doc: add comments
    Exercises manipulating text.
    Searching for a string within another.

--- ex025.py
+++ ex025.py
@@ -1,3 +1,10 @@
+#Searching for a string within another
 #25- Create a program that reads a person's name and tells if they have "Silva" in their name
 name = str(input('What your full name?'))
 print(F'Your name has Silva? {'silva' in name.lower()}.')
+
+#Procurando uma string dentro de outra
+#25: Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+'''nome = str(input('Qual é o seu nome completo?'))
+print(f'No seu nome tem Silva? {'silva' in name.lower()}')'''
+
