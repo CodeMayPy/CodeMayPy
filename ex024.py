3fa33b377f874eb2ad32e76fc98c1cc3ff5d8433
Mayara <code.maypy@gmail.com>
2025-01-13 10:39:15 -0300

    doc: add comments
    Exercises manipulating text.
    Checking the first letters of a text.

--- ex024.py
+++ ex024.py
@@ -1,3 +1,4 @@
+#Checking the first letters of a text
  #24- Create a program that reads the name of a city and says whether or not it starts with the name 'Saint'.
 from time import sleep
 city = str(input('What city were you born?')).strip()
@@ -5,6 +6,7 @@ print('Has a Saint in the name?)
 sleep(1)
 print(city[:5].upper() == 'SAINT')
 
+#Verificando as primeiras letras de um texto
 #024: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
 '''cidade = str(input('Digite o nome de uma cidade:')).strip()
 print('Tem Santo no nome?')
