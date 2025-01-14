a915cba56d12957205e755ec42397c3b8f2f80c5
Mayara <code.maypy@gmail.com>
2025-01-14 10:52:27 -0300

    doc: add comments
    aligned conditions exercuses.
    Number base converter.

--- ex037.py
+++ ex037.py
@@ -1,3 +1,4 @@
+#Number Base Converter
 #37-Write a Python program that reads any integer and asks the user to choose the conversion base: 1 for binary, 2 for
 # octal, and 3 for hexadecimal
 number= int(input('Enter an integer:'))
@@ -14,3 +15,27 @@ elif option == 3:
     print(f'{number} converted to hexadecimal it becomes {hex(number)[2:]}.')
 else:
     print('Invalid option try again!')
+
+
+
+#Conversor de Bases Numéricas
+#37: Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de
+#conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
+numero = int(input('Digite um número inteiro qualquer:'))
+print('''Escolha uma das bases de conversão:
+      [ 1 ] BINÁRIO'
+      [ 2 ] OCTAL'
+      [ 3 ] HEXADECIMAL''')
+opcao = int(input('Digite sua escolha:'))
+if opcao == 1:
+    print(f'o número {numero} convertido para binário fica: {bin(numero)[2:]}.')
+elif opcao == 2:
+    print(f'O número {numero} convertido para octal fica: {oct(numero)[2:]}.')
+elif opcao == 3:
+    print(f'O número {numero} convertido para hexadecimal fica: {hex(numero)[2:]}.')
+else:
+    print('Opção inválida, tente novamente.')
+
+
+#PT- Condições aninhadas.
+#EN- Aligned conditions.
