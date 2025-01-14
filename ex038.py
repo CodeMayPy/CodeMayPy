4348a30814b2d2f71aeae271981265e667ffa1fc
Mayara <code.maypy@gmail.com>
2025-01-14 11:00:54 -0300

    doc: add comments
    Aligned conditions exercises.
    Comparing numbers.

--- ex038.py
+++ ex038.py
@@ -1,3 +1,4 @@
+#Comparing numbers
 #38- Write a program that reads two integers and compares them, displaying a message on the screen:
 # – The first value is greater
 # – The second value is greater
@@ -10,3 +11,21 @@ elif first_number < second_number:
     print(f'The number {second_number} is greater than {first_number}.')
 else:
     print('The two numbers are the same!')
+
+
+#Comparando números
+#38: Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
+#O primeiro valor é maior
+#O segundo valor é maior
+#Não existe valor maior, os dois são iguais
+primeiro = int(input('Digite um número:'))
+segundo = int(input('Digite outro número:'))
+if primeiro > segundo:
+    print(f'O número {primeriro} é maior que {segundo}.')
+elif segundo > primeiro:
+    print(f'O número {segundo} é maior que {primeiro}.')
+else:
+    print('Os dois números são iguais.')
+
+#PT- Condições aninhadas.
+#EN- Aligned conditions.
