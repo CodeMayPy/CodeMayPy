c6880bcaccf6df4e3d89b10860f34c0a32359d91
Mayara <code.maypy@gmail.com>
2025-01-15 12:17:02 -0300

    doc: add comments.
    Aligned conditions exercises.
    Game: rock, paper or scissors.

--- ex045.py
+++ ex045.py
@@ -1,3 +1,4 @@
+#GAME: Rock Paper Scissors
 #45-Create a program that makes the computer play Jokenpô with you.
 from random import randint
 print("Let's play rock, paper, scissors:")
@@ -39,3 +40,49 @@ elif computer == 2:
          print('Draw')
     else:
         print('Invalid move!')
+
+
+#GAME: Pedra Papel e Tesoura
+#45: Crie um programa que faça o computador jogar Jokenpô com você.
+from random import randint
+print('Vamos jogar jokenpô')
+items= ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura''')
+jogador = int(input('Qual a sua jogada?'))
+print(f'computador jogou {items[computador]}.')
+print(f'Você jogou {items[jogador]}.')
+if computador == 0:
+   if jogador == 0:
+      print('Empate.')
+   elif jogador == 1:
+       print('Você ganhou!')
+   elif jogador == 2:
+        print('Computador ganhou!')
+   else:
+       print('Jogada inválida!')
+elif computador == 1:
+    if jogador == 0:
+      print('Computador ganhou!')
+    elif jogador == 1:
+      print('Empate!')
+    elif jogador == 2:
+      print('Você ganhou!')
+    else:
+        print('Jogada inválida!')
+elif computador == 2:
+    if jogador == 0:
+       print('Você ganhou!')
+    elif jogador == 1:
+         print('Computador ganhou!')
+    elif jogador == 2:
+         print('Empate!')
+    else:
+        print('Jogada inválida!')
+
+
+#PT- Condições aninhadas.
+#EN- Aligned conditions.
