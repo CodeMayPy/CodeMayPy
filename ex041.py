940859e90332a5b64caab8ecdac32424ce1ad3ef
Mayara <code.maypy@gmail.com>
2025-01-15 10:32:23 -0300

    doc: add comments.
    Aligned conditions exercises.
    Ranking athletes.

--- ex041.py
+++ ex041.py
@@ -1,3 +1,4 @@
+#Ranking Athletes
 #41-The National Swimming Confederation needs a program that reads an athlete's birth year and shows their
 # category, according to age: – Up to 9 years old: child
 #– Up to 14 years old: children's
@@ -19,3 +20,31 @@ if age >= 20 and age <= 25:
     print('Your category is SENIOR!')
 elif age >=26:
     print('Your category is MASTER!')
+
+
+#Classificando Atletas
+#041: A Confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um atleta e mostre
+#sua categoria, de acordo com a idade:
+# Até 9 anos: MIRIM
+# Até 14 anos: INFANTIL
+# Até 19 anos: JÚNIOR
+# Até 25 anos: SÊNIOR
+# Acima de 25 anos: MASTER
+from datetime import date
+nascimento = int(input('Qual o seu ano de nascimento?'))
+ano = date.today().year
+idade = ano - nascimento
+if idade <= 9:
+    print(f'Sua idade é {idade} então sua categoria é a mirim.')
+if idade > 9 and idade <= 14:
+    print(f'Sua idade é {idade} então sua categoria é a infantil.')
+if idade  > 14 and idade <= 19:
+    print(f'Sua idade é {idade} então sua categoria é a junior.')
+if idade > 19 and idade <= 25:
+    print(f'Sua idade é {idade} então sua categoria é a sênior.')
+elif idade >= 26:
+    print(f'Sua idade é {idade} e sua categoria é a master.')
+
+
+#PT- Condições aninhadas.
+#EN- Aligned conditions.
