7a2842064717513f16302fb40dce6646208a5a3e
Mayara <code.maypy@gmail.com>
2025-01-15 11:29:44 -0300

    doc: add comments.
    Aligned conditions exercises.
    Body mass index.

--- ex043.py
+++ ex043.py
@@ -1,3 +1,4 @@
+#Body Mass Index
 #43-Develop a logic that reads a person's weight and height, calculates their Body Mass Index (BMI) and
 # shows their status, according to the table below:
 # – BMI below 18.5: Underweight
@@ -19,3 +20,30 @@ elif 30 <= 40:
     print('You are obesity.')
 elif bmi >= 40:
     print('You are morbidly obese.')
+
+
+#Índice de Massa Corporal
+# 43: Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre
+#seu status, de acordo com a tabela abaixo:
+# IMC abaixo de 18,5: Abaixo do Peso
+# Entre 18,5 e 25: Peso Ideal
+# 25 até 30: Sobrepeso
+# 30 até 40: Obesidade
+# Acima de 40: Obesidade Mórbida
+peso = float(input('Qual é o seu peso em KG:'))
+altura = float(input('Qual é a sua altura em metros?'))
+imc = peso / (altura ** 2)
+if imc < 18.5:
+    print(f'Seu IMC é {imc:.2f} e você está abaixo do peso.')
+elif imc >= 18.5 and imc <25:
+    print(f'Seu IMC é {imc:.2f} e você está no seu peso ideal.')
+elif imc >= 25 and imc < 30:
+    print(f'Seu IMC é de {imc:.2f} e você está com sobrepeso.')
+elif imc >= 30 and imc <= 40:
+    print(f'Seu IMC é de {imc:.2f} e você está com obesidade.')
+elif imc > 40:
+    print(f'Seu IMC é de {imc:.2f} e você está com obesidade mórbida.')
+
+
+# PT- Condições aninhadas.
+# EN- Aligned conditions.
