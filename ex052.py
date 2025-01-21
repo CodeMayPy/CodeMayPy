b70a86166c279c6fbeafc5772fe634d8a2d9149e
Mayara <code.maypy@gmail.com>
2025-01-21 12:43:52 -0300

    doc: add comments
    Loop structure: for.
    Prime numbers.

--- ex052.py
+++ ex052.py
@@ -1,3 +1,4 @@
+#Prime numbers
 #52- Write a program that reads an integer and tells whether or not it is a prime number.
 number = int(input('Enter a integer number:'))
 tot = 0
@@ -13,3 +14,22 @@ if tot == 2:
     print('And that is why he is a prime number.')
 else:
     print('And that is why it is not a prime number.')
+
+
+#Números primos
+#52: Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+numero = int(input('Digite um número:'))
+total = 0
+for contador in range(1,numero,+1):
+    if numero % contador == 0:
+        print(end='')
+        total += 1
+    else:
+        print(end='')
+if total == 2:
+    print('O número é primo.')
+else:
+    print('O número é impar.')
+
+# PT- Estruturas de repetições: For.
+# EN- Loop structures: For.
