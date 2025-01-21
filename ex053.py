d5a809887c821632f04074144be62efe4093b35d
Mayara <code.maypy@gmail.com>
2025-01-21 12:45:11 -0300

    doc: add comments
    Loop structure: for.
    Palindrome detector.

--- ex053.py
+++ ex053.py
@@ -1,7 +1,5 @@
-#53- Create a program that reads any sentence and says if it is a palindrome, disregarding spaces. Examples of
-# palindromes:
-# AFTER THE SOUP, THE BALCONY OF THE HOUSE, THE TOWER OF DEFEAT, THE WOLF LOVES THE CAKE, THEY WROTE DOWN THE DATE
-# OF THE MARATHON.
+#Palindrome Detector
+#53- Create a program that reads any sentence and says if it is a palindrome, disregarding spaces.
 sentence = str(input('Enter a sentence:')).strip().upper()
 words = sentence.split()
 together = ''.join(words)
@@ -13,3 +11,22 @@ if reverse == together:
     print('We have a palindrome.')
 else:
     print('The sentence is not a palindrome.')
+
+
+#Detector de Palíndromo
+#53: Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços. Exemplos
+# de palíndromos:APÓS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA.
+frase = str(input('Digite uma frase:')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for palavras in range(len(junto)-1,-1,-1):
+    inverso += junto[palavras]
+if inverso == junto:
+    print('Nós temos um palíndromo.')
+else:
+    print('Nós não temos um palíndromo.')
+
+
+#PT- Estruturas de repetições: For.
+#EN- Loop structures: For.
