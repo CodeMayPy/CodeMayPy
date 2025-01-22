3f9c4eb661c8c168b290df28a93a9e694147199a
Mayara <code.maypy@gmail.com>
2025-01-22 15:50:33 -0300

    doc: add comments.
    Loop structure: While.
    Guessing game v2.0

--- ex058.py
+++ ex058.py
@@ -1,6 +1,8 @@
+#EN- Loop structure: While
+#Guessing Game v2.0
 #58- Improve the challenge 28 game where the computer will “think” of a number between 0 and 10. But now the player will
 #try to guess until you get it right, showing at the end how many guesses it took to win.
-from random import randint
+'''from random import randint
 computer = randint(0,10)
 print('I am your computer. . . I just thought of a number between 0 and 10. ')
 print('Can you guess which one it was?')
@@ -16,4 +18,26 @@ while not got_it_right:
             print('More. Try again.')
         elif player > computer:
             print('Less. Try again.')
-print(f'You got it rigth with {guesses} attempts. Congratulations!')
+print(f'You got it rigth with {guesses} attempts. Congratulations!')'''
+
+
+#PT- Estrutura de repetição: While.
+#Jogo da Adivinhação v2.0
+#58: Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10. Só que agora o jogador vai
+# tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+from random import randint
+computador = randint(0,10)
+print('Eu estou pensando em um número de 0 a 10, tente adivinhar qual é!')
+acerto = False
+tentativa = 0
+while not acerto:
+    jogador = int(input('Qual o seu palpite?'))
+    tentativa += 1
+    if jogador == computador:
+        acerto = True
+    else:
+        if jogador < computador:
+            print('Tente novamente.')
+        elif jogador > computador:
+            print('Quase, tente denovo.')
+print(f'Você acertou na sua {tentativa}ª. Parabéns')
