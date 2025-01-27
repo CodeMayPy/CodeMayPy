da6b7bcb1539b82e286c73a975c141150a6f0cbf
Mayara <code.maypy@gmail.com>
2025-01-27 17:41:50 -0300

    doc: add comments
    Loop structure: While.
    Arithmetic progression v2.0

--- ex061.py
+++ ex061.py
@@ -1,5 +1,6 @@
-#61-  Redo CHALLENGE 51, reading the first term and the reason of an AP, 
-showing the first 10 terms of the progression
+#EN- Loop structure: While
+#Arithmetic Progression v2.0
+#61-  Redo CHALLENGE 51, reading the first term and the reason of an AP,showing the first 10 terms of the progression
 #  using the while structure.
 print("Arithmetic progression generator")
 print("-=-" * 20)
@@ -12,3 +13,17 @@ while counter <= 10:
     term += reason
     counter += 1
 print("End")
+
+#PT- Estruturas de repetição: While.
+#Progressão Aritmética v2.0
+#61: Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão
+# usando a estrutura while.
+primeiro_termo = int(input('Digite o primeiro valor:'))
+razao = int(input('Qual a razão da PA?'))
+termo = primeiro_termo
+contador = 1
+while contador <= 10:
+    print(f'{termo}', end=' ')
+    termo += razao
+    contador += 1
+
